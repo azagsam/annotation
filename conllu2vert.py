07946afd0a5845f05c89e58dec22905eead77146
Ales Zagar <ales.zagar@hotmail.com>
2022-05-31 13:26:03 +0200

    sent_id + par_id

--- conllu2vert.py
+++ conllu2vert.py
@@ -10,9 +10,10 @@ def reorder_tokens(output_tokens):
         reordered_output.append(reordered)
     return reordered_output
 
+
 n = 208
 file = f'data/maks/maks.conllu-jos_standard-slo/maks{n}.conllu'
-with open(file, 'r') as f, open(f'maks{n}-v2.vert', 'w') as out:
+with open(file, 'r') as f, open(f'maks{n}-v3.vert', 'w') as out:
     sentences = parse(f.read())
     for sentence in sentences:
         sentence_keys = sentence.metadata.keys()
@@ -26,15 +27,16 @@ with open(file, 'r') as f, open(f'maks{n}-v2.vert', 'w') as out:
         # write paragraph tags
         if 'newpar id' in sentence_keys:
             newpar_id = int(sentence.metadata['newpar id'].split('.')[-1])
+            par_id = sentence.metadata["newpar id"]
             # catch first paragraph
             if newpar_id == 1:
-                out.write('<p>')
+                out.write(f'<p id=\"{par_id}\">')
                 out.write('\n')
             # catch middle paragraphs
             else:
                 out.write('</p>')
                 out.write('\n')
-                out.write('<p>')
+                out.write(f'<p id=\"{par_id}\">')
                 out.write('\n')
 
         # transform tokens back to original conllu
@@ -42,7 +44,8 @@ with open(file, 'r') as f, open(f'maks{n}-v2.vert', 'w') as out:
         output_tokens = reorder_tokens(output_tokens)
 
         # start writing a sentence
-        out.write('<s>')
+        sent_id = sentence.metadata["sent_id"]
+        out.write(f'<s id=\"{sent_id}\">')
         out.write('\n')
 
         # extract text
