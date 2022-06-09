53e80b41e01af3f37906645152a89f93cb5dc6e2
Ales Zagar <ales.zagar@hotmail.com>
2022-06-09 08:48:40 +0200

    maks2txt function

--- maks.py
+++ maks.py
@@ -2,7 +2,34 @@ import os
 from conllu import parse
 from tqdm import tqdm
 import classla
-from stanza_batch import batch
+from collections import defaultdict
+
+
+def conllu2txt(source, output):
+    # fetch document
+    for src_file in tqdm(os.listdir(source)):
+        if src_file in list(os.listdir(output)):
+            continue
+        doc_id = src_file.split('.')[0]
+        src_path = os.path.join(source, src_file)
+
+        # parse document
+        parsed_text = ""
+        with open(src_path, 'r', encoding="utf-8") as f:
+            sentences = parse(f.read())
+            for sent in sentences:
+                text = sent.metadata['text']
+                if not parsed_text:
+                    parsed_text += text
+                elif 'newpar id' in sent.metadata.keys():
+                    parsed_text += '\n\n' + text
+                else:
+                    parsed_text += ' ' + text
+
+        # write txt
+        out_path = os.path.join(output, doc_id + '.txt')
+        with open(out_path, 'w', encoding='utf-8') as out:
+            out.write(parsed_text)
 
 
 def old2new(source, output):
