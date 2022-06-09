2b044a939d7c1b6be838e8898dcf85fda94188e8
Ales Zagar <ales.zagar@hotmail.com>
2022-06-09 10:35:27 +0200

    reduce maks in size

--- maks.py
+++ maks.py
@@ -1,8 +1,11 @@
 import os
+
+import json
 from conllu import parse
 from tqdm import tqdm
 import classla
 from collections import defaultdict
+import numpy as np
 
 
 def conllu2txt(source, output):
@@ -82,6 +85,52 @@ def old2new(source, output):
                 print('Other type of error')
 
 
+def reduce_doc_size(source, target):
+    # count tokens in each document
+    counter = defaultdict(int)
+    with open(source, 'r') as f:
+        for line in tqdm(f):
+            if line.startswith('<text id'):
+                doc_id = line.split()[1].split('=')[1][1:-1]
+            elif len(line.split('\t')) == 10:
+                counter[doc_id] += 1
+    reduced_sizes = {doc: int(np.ceil(x * 0.1)) for doc, x in counter.items()}  # 10% of each document
+
+    # find sent_ids that exceed reduced sizes
+    sent_ids = {}
+    with open(source, 'r') as f:
+        for line in tqdm(f):
+            if line.startswith('<text id'):
+                n = 0
+                doc_id = line.split()[1].split('=')[1][1:-1]
+            elif len(line.split('\t')) == 10:
+                n += 1
+                if n > reduced_sizes[doc_id] and doc_id not in sent_ids.keys():
+                    sent_id = ".".join(line.split('\t')[9].split('.')[:-1])
+                    sent_ids[doc_id] = sent_id
+
+    # write to target
+    with open(source, 'r') as f, open(target, 'w') as out:
+        for line in tqdm(f):
+            if line.startswith('<text id'):
+                writing_mode = True  # flag for writing/skipping lines from a source
+                out.write(line)
+                doc_id = line.split()[1].split('=')[1][1:-1]
+
+            elif writing_mode:
+                if line.startswith('<s id'):  # check if new sentence is starting
+                    sent_id = line[7:-3]
+                    if sent_ids[doc_id] == sent_id:  # check if a new sentence is the same as the sentence that exceeds 10%
+                        # turn off writing mode and write closing tags
+                        out.write('</p>\n')
+                        out.write('</text>\n')
+                        writing_mode = False
+                    else:
+                        out.write(line)
+                else:
+                    out.write(line)
+
+
 if __name__ == '__main__':
     # import annotator
     classla.download('sl', type='standard_jos')
@@ -99,3 +148,10 @@ if __name__ == '__main__':
 
     # annotate
     old2new(source='data/maks/maks.conllu-v1', output='data/maks/maks.conllu-v2')
+
+    # convert2txt
+    conllu2txt('/home/azagar/myfiles/annotation/data/maks/maks.conllu-original', '/home/azagar/myfiles/annotation/data/maks/maks.txt')
+
+    # select 10% of each element
+    reduce_doc_size('/home/azagar/myfiles/annotation/data/maks/maks4list.vert/maks4list.vert',
+                    '/home/azagar/myfiles/annotation/data/maks/maks4list.vert/maks4list.smaller.vert')
