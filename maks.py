8df4011852b5d44c11e830bae3cc5e18fc6fb0dd
Ales Zagar <ales.zagar@hotmail.com>
2022-08-03 14:58:51 +0200

    new and modified functions for cc versio

--- maks.py
+++ maks.py
@@ -6,6 +6,10 @@ from tqdm import tqdm
 import classla
 from collections import defaultdict
 import numpy as np
+import re
+import shutil
+
+import tempfile
 
 
 def conllu2txt(source, output):
@@ -85,7 +89,7 @@ def old2new(source, output):
                 print('Other type of error')
 
 
-def reduce_doc_size(source, target):
+def reduce_doc_size(source, target, skip=None, write_last_sentence_ids_ccmaks=None):
     # count tokens in each document
     counter = defaultdict(int)
     with open(source, 'r') as f:
@@ -107,9 +111,23 @@ def reduce_doc_size(source, target):
                 n += 1
                 if n > reduced_sizes[doc_id] and doc_id not in sent_ids.keys():
                     sent_id = ".".join(line.split('\t')[9].split('.')[:-1])
+                    par_id, s_id = int(sent_id.split('.')[1]),  int(sent_id.split('.')[2])
+                    if par_id == 1 and s_id == 1:  # include the first sentence, even if it exceeds 10% of the text
+                        continue
                     sent_ids[doc_id] = sent_id
 
+    for doc_id in counter.keys():  # one document contains only one sentence
+        if doc_id not in sent_ids.keys():
+            print(doc_id)
+            sent_ids[doc_id] = doc_id+'.1.2'  # something other than x.1.1
+
+    if write_last_sentence_ids_ccmaks:
+        with open(write_last_sentence_ids_ccmaks, 'w') as f:
+            for doc_id, sent_id in sent_ids.items():
+                f.write(f"{doc_id}\t{sent_id}\n")
+
     # write to target
+    non_skipped_files_ids = []
     with open(source, 'r') as f, open(target, 'w') as out:
         for line in tqdm(f):
             if line.startswith('<text id'):
@@ -120,8 +138,9 @@ def reduce_doc_size(source, target):
             elif writing_mode:
                 if line.startswith('<s id'):  # check if new sentence is starting
                     sent_id = line[7:-3]
-                    if sent_ids[doc_id] == sent_id:  # check if a new sentence is the same as the sentence that exceeds 10%
-                        # turn off writing mode and write closing tags
+                    if sent_ids[doc_id] == sent_id and doc_id not in skip:  # check if a new sentence is the same as the sentence that exceeds 10% and not in a skip list
+                        # turn off writing mode for files that are not in skip and write closing tags
+                        non_skipped_files_ids.append(doc_id)
                         out.write('</p>\n')
                         out.write('</text>\n')
                         writing_mode = False
@@ -129,29 +148,109 @@ def reduce_doc_size(source, target):
                         out.write(line)
                 else:
                     out.write(line)
+    print('Non-skipped files:', len(non_skipped_files_ids))
+
+
+def build_ccmaks_vert():
+    # paths
+    ccgigafida_maks = '/home/azagar/myfiles/annotation/data/ccgigafida/ccgigafida_ccmaks4list_unconcatenated.vert'
+    maks4list_unconcatenated = '/home/azagar/myfiles/annotation/data/ccmaks/maks4list_unconcatenated.vert'
+    maks_header = '/home/azagar/myfiles/annotation/data/maks/maks_header.txt'
+
+    # gather all documents
+    docs = {}
+    ccgigafida_maks_ids = set()
+    for file in os.listdir(ccgigafida_maks):  # add gigafida docs
+        full_path = os.path.join(ccgigafida_maks, file)
+        with open(full_path, 'r') as f:
+            lines = f.readlines()
+            doc_id = file.split('.')[0]
+            ccgigafida_maks_ids.add(doc_id)
+            docs[doc_id] = lines
+
+    for file in os.listdir(maks4list_unconcatenated):  # add maks docs
+        doc_id = file.split('.')[0]
+        if doc_id not in docs.keys():  # skip gigafida docs
+            full_path = os.path.join(maks4list_unconcatenated, file)
+            with open(full_path, 'r') as f:
+                lines = f.readlines()
+                docs[doc_id] = lines
+
+    # get metadata
+    metadata = {}
+    with open(maks_header, 'r') as h:
+        for line in h:
+            doc_id = line.split()[1].split('=')[1][1:-1]
+            metadata[doc_id] = line
+
+    docs_with_metadata = {}
+    for doc_id, lines in docs.items():
+        docs_with_metadata[doc_id] = [metadata[doc_id]] + lines[1:]  # replace first line with metadata line
+
+    # create temp file for reducing doc size
+    tmp = tempfile.NamedTemporaryFile()
+    sorted_docs = sorted(docs_with_metadata.keys(), key=lambda x: int(re.findall(r'\d+', x)[0]))
+    for doc_id in sorted_docs:
+        lines = docs_with_metadata[doc_id]
+        for line in lines:
+            tmp.write(line.encode('utf-8'))
+
+    reduce_doc_size(tmp.name, '/home/azagar/myfiles/annotation/data/ccmaks/ccmaks4list.vert/ccmaks4list.vert', skip=ccgigafida_maks_ids,
+                    write_last_sentence_ids_ccmaks='/home/azagar/myfiles/annotation/data/ccmaks/ccmaks4list_last_sentence_ids.txt')
+
+
+def build_ccmaks_conllu():
+    data = {}
+    with open('/home/azagar/myfiles/annotation/data/ccmaks/ccmaks4list_last_sentence_ids.txt', 'r') as f:
+        for line in f:
+            doc_id, sent_id = line.strip().split('\t')
+            data[doc_id] = sent_id
+
+    gigafida_ids = [file.split('.')[0] for file in os.listdir('/home/azagar/myfiles/annotation/data/ccgigafida/ccgigafida_ccmaks4list_unconcatenated.vert')]
+    source = '/home/azagar/myfiles/annotation/data/ccmaks/maks.conllu-jos_standard-slo'
+    target = '/home/azagar/myfiles/annotation/data/ccmaks/ccmaks.conllu'
+    for file in os.listdir(source):
+        source_path = os.path.join(source, file)
+        target_path = os.path.join(target, file)
+        doc_id = file.split('.')[0]
+        if doc_id in gigafida_ids:
+            shutil.copyfile(source_path, target_path)
+        else:
+            with open(source_path, 'r') as f, open(target_path, 'w') as out:
+                sentences = parse(f.read())
+                for sent in sentences:
+                    sent_id = sent.metadata['sent_id']
+                    if sent_id == data[doc_id]:
+                        break
+                    else:
+                        out.write(sent.serialize())
 
 
 if __name__ == '__main__':
-    # import annotator
-    classla.download('sl', type='standard_jos')
-    nlp = classla.Pipeline('sl',
-                           processors="tokenize,pos,ner,lemma,depparse",
-                           type='standard_jos',
-                           pos_use_lexicon=True,
-                           # use_gpu=False
-                           # tokenize_batch_size=32,
-                           # ner_batch_size=32,
-                           # pos_batch_size=5000,
-                           # lemma_batch_size=50,
-                           # depparse_batch_size=5000
-                           )
+    # # import annotator
+    # classla.download('sl', type='standard_jos')
+    # nlp = classla.Pipeline('sl',
+    #                        processors="tokenize,pos,ner,lemma,depparse",
+    #                        type='standard_jos',
+    #                        pos_use_lexicon=True,
+    #                        # use_gpu=False
+    #                        # tokenize_batch_size=32,
+    #                        # ner_batch_size=32,
+    #                        # pos_batch_size=5000,
+    #                        # lemma_batch_size=50,
+    #                        # depparse_batch_size=5000
+    #                        )
+    #
+    # # annotate
+    # old2new(source='data/maks/maks.conllu-v1', output='data/maks/maks.conllu-v2')
+    #
+    # # convert2txt
+    # conllu2txt('/home/azagar/myfiles/annotation/data/maks/maks.conllu-original', '/home/azagar/myfiles/annotation/data/maks/maks.txt')
+    #
+    # # select 10% of each element
+    # reduce_doc_size('/home/azagar/myfiles/annotation/data/maks/maks4list.vert/maks4list.vert',
+    #                 '/home/azagar/myfiles/annotation/data/maks/maks4list.vert/maks4list.smaller.vert')
 
-    # annotate
-    old2new(source='data/maks/maks.conllu-v1', output='data/maks/maks.conllu-v2')
-
-    # convert2txt
-    conllu2txt('/home/azagar/myfiles/annotation/data/maks/maks.conllu-original', '/home/azagar/myfiles/annotation/data/maks/maks.txt')
-
-    # select 10% of each element
-    reduce_doc_size('/home/azagar/myfiles/annotation/data/maks/maks4list.vert/maks4list.vert',
-                    '/home/azagar/myfiles/annotation/data/maks/maks4list.vert/maks4list.smaller.vert')
+    # create ccmaks
+    build_ccmaks_vert()
+    build_ccmaks_conllu()
